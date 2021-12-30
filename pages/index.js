7959e929ccc4d93b8bae50264aae10830f3fa439
Nicksonder Examar <nextgendev12@gmail.com>
2021-12-30 11:10:05 -0500

    Finished with the Home page, moving on to the Stories page.

--- pages/index.js
+++ pages/index.js
@@ -35,10 +35,10 @@ export default function Home() {
           content="PhotoSnap, photo, pictures, beautifull photo"
         />
         <meta name="author" content="Nicksonder Examar" />
-        <title>PhotoSnap - Post and get inspired from beautifull photos.</title>
+        <title>PhotoSnap - Post and get inspired from beautiful photos.</title>
       </Head>
 
-      {/* Header Section */}
+      {/************* Header Section *****************/}
       <header className="header">
         <div className="container">
           <nav className="header__nav-bar">
@@ -73,12 +73,12 @@ export default function Home() {
         </div>
       </header>
 
-      {/* Main Section */}
+      {/* Main Section *****************/}
       <main className="main">
-        {/* Top Section */}
+        {/* Top Section *****************/}
         <section className="top-section">
           <div className="top-section__container">
-            <div className="top-section__top-wrapper">
+            <div className="top-section__wrapper">
               <div className="top-section__colored-bar"></div>
               {/* Top Info */}
               <div className="top-section__info">
@@ -97,7 +97,8 @@ export default function Home() {
                     <div className="top-section__button">Get an Invite</div>
                     <img
                       className="top-section__right-arrow"
-                      src="assets/arrow-right.png"
+                      src="assets/shared/desktop/arrow.svg"
+                      alt="Right Arrow"
                     />
                   </a>
                 </div>
@@ -109,76 +110,372 @@ export default function Home() {
           </div>
         </section>
 
-        {/* Stories Section */}
-        <section className="story-section">
-          <div className="story-section__container">
-            <div className="story-section__top-wrapper">
-              {/* Story Image */}
-              <div className="story-section__img"></div>
+        {/* Stories Template Section *****************/}
+        <section className="story-template">
+          <div className="story-template__wrapper">
+            {/* Story Image */}
+            <div className="story-template__img"></div>
 
-              {/* Story Info */}
-              <div className="story-section__info">
-                <div className="story-section__info-wrapper">
-                  <h1 className="story-section__title">
-                    Beautiful Stories Everytime
-                  </h1>
+            {/* Story Info */}
+            <div className="story-template__info">
+              <div className="story-template__info-wrapper">
+                <h1 className="story-template__title">
+                  Beautiful Stories Every time
+                </h1>
 
-                  <p className="story-section__p">
-                    We provide design templates to ensure your stories look
-                    terrific. Easily add photos, text, embed maps and media from
-                    other networks. Then share your story with everyone.
-                  </p>
+                <p className="story-template__p">
+                  We provide design templates to ensure your stories look
+                  terrific. Easily add photos, text, embed maps and media from
+                  other networks. Then share your story with everyone.
+                </p>
 
-                  <a href="#">
-                    <div className="story-section__button">
-                      View The Stories
-                    </div>
-                    <img
-                      className="story-section__right-arrow"
-                      src="assets/arrow-right-black.png"
-                    />
-                  </a>
-                </div>
+                <a href="#">
+                  <div className="story-template__button">View The Stories</div>
+                  <img
+                    className="story-template__right-arrow"
+                    src="assets/shared/desktop/arrow.svg"
+                    alt="Right Arrow"
+                  />
+                </a>
               </div>
             </div>
           </div>
         </section>
 
-        {/* Design Section */}
+        {/* Design Section *****************/}
         <section className="design-section">
-          <div className="design-section__container">
-            <div className="design-section__top-wrapper">
-              {/* Design Info */}
-              <div className="design-section__info">
-                <div className="design-section__info-wrapper">
-                  <h1 className="design-section__title">
-                    Designed for everyone
-                  </h1>
+          <div className="design-section__wrapper">
+            {/* Design Info */}
+            <div className="design-section__info">
+              <div className="design-section__info-wrapper">
+                <h1 className="design-section__title">Designed for everyone</h1>
 
-                  <p className="design-section__p">
-                    Photosnap can help you create stories that resonate with
-                    your audience. Our tool is designed for photographers of all
-                    levels, brands, businesses you name it.
-                  </p>
+                <p className="design-section__p">
+                  Photosnap can help you create stories that resonate with your
+                  audience. Our tool is designed for photographers of all
+                  levels, brands, businesses you name it.
+                </p>
 
-                  <a href="#">
-                    <div className="design-section__button">
-                      View The Stories
-                    </div>
+                <a href="#">
+                  <div className="design-section__button">View The Stories</div>
+                  <img
+                    className="design-section__right-arrow"
+                    src="assets/shared/desktop/arrow.svg"
+                    alt="Right Arrow"
+                  />
+                </a>
+              </div>
+            </div>
+
+            {/* Design Image */}
+            <div className="design-section__img"></div>
+          </div>
+        </section>
+
+        {/* Story Section *****************/}
+        <section className="story-section">
+          <div className="story-section__wrapper">
+            {/* First item */}
+            <div className="story-section__item">
+              <a href="#" className="story-section__item-1">
+                <div className="story-section__item-wrapper">
+                  <h3 className="story-section__title">The Mountains</h3>
+                  <p className="story-section__name">by John Appleseed</p>
+                  <hr />
+                  <div className="story-section__button">
+                    Read Story
                     <img
-                      className="design-section__right-arrow"
-                      src="assets/arrow-right-black.png"
+                      className="story-section__right-arrow"
+                      src="assets/shared/desktop/arrow.svg"
+                      alt="Right Arrow"
                     />
-                  </a>
+                  </div>
                 </div>
+                <div className="story-section__border-bottom"></div>
+              </a>
+            </div>
+
+            {/* Second item */}
+            <div className="story-section__item">
+              <a href="#" className="story-section__item-2">
+                <div className="story-section__item-wrapper">
+                  <h3 className="story-section__title">Sunset Cityscapes</h3>
+                  <p className="story-section__name">by Benjamin Cruz</p>
+                  <hr />
+                  <div className="story-section__button">
+                    Read Story
+                    <img
+                      className="story-section__right-arrow"
+                      src="assets/shared/desktop/arrow.svg"
+                      alt="Right Arrow"
+                    />
+                  </div>
+                </div>
+                <div className="story-section__border-bottom"></div>
+              </a>
+            </div>
+
+            {/* Third item */}
+            <div className="story-section__item">
+              <a href="#" className="story-section__item-3">
+                <div className="story-section__item-wrapper">
+                  <h3 className="story-section__title">18 Days Voyage</h3>
+                  <p className="story-section__name">by Alexei Borodin</p>
+                  <hr />
+                  <div className="story-section__button">
+                    Read Story
+                    <img
+                      className="story-section__right-arrow"
+                      src="assets/shared/desktop/arrow.svg"
+                      alt="Right Arrow"
+                    />
+                  </div>
+                </div>
+                <div className="story-section__border-bottom"></div>
+              </a>
+            </div>
+
+            {/* Fourth item */}
+            <div className="story-section__item ">
+              <a href="#" className="story-section__item-4">
+                <div className="story-section__item-wrapper">
+                  <h3 className="story-section__title">Architecturals</h3>
+                  <p className="story-section__name">by Samantha Brooke</p>
+                  <hr />
+                  <div className="story-section__button">
+                    Read Story
+                    <img
+                      className="story-section__right-arrow"
+                      src="assets/shared/desktop/arrow.svg"
+                      alt="Right Arrow"
+                    />
+                  </div>
+                </div>
+                <div className="story-section__border-bottom"></div>
+              </a>
+            </div>
+          </div>
+        </section>
+
+        {/* Features Section *****************/}
+        <section className="features-section">
+          <div className="container">
+            <div className="features-section__wrapper">
+              {/* First item */}
+              <div className="features-section__item">
+                <div className="features-section__img">
+                  <img
+                    src="assets/features/desktop/responsive.svg"
+                    alt="Responsive icon"
+                  />
+                </div>
+
+                <h3 className="features-section__title">100% Responsive</h3>
+
+                <p className="features-section__p">
+                  No matter which the device you’re on, our site is fully
+                  responsive and stories look beautiful on any screen.
+                </p>
               </div>
 
-              {/* Design Image */}
-              <div className="design-section__img"></div>
+              {/* Second item */}
+              <div className="features-section__item">
+                <div className="features-section__img">
+                  <img
+                    src="assets/features/desktop/no-limit.svg"
+                    alt="No limit icon"
+                  />
+                </div>
+
+                <h3 className="features-section__title">
+                  No Photo Upload Limit
+                </h3>
+
+                <p className="features-section__p">
+                  Our tool has no limits on uploads or bandwidth. Freely upload
+                  in bulk and share all of your stories in one go.
+                </p>
+              </div>
+
+              {/* Third item */}
+              <div className="features-section__item">
+                <div className="features-section__img">
+                  <img
+                    src="assets/features/desktop/embed.svg"
+                    alt="Embed icon"
+                  />
+                </div>
+
+                <h3 className="features-section__title">Available to Embed</h3>
+
+                <p className="features-section__p">
+                  Embed Tweets, Facebook posts, Instagram media, Vimeo or
+                  YouTube videos, Google Maps, and more.
+                </p>
+              </div>
             </div>
           </div>
         </section>
       </main>
+
+      {/************* Footer *****************/}
+      <footer className="footer">
+        <div className="container">
+          <div className="footer__content-wrapper">
+            <div className="footer__left-side">
+              <div className="footer__logo">
+                <a href="#">
+                  <img
+                    className="footer__logo-img"
+                    src="assets/logo-white.svg"
+                  />
+                </a>
+              </div>
+
+              <div className="footer__social">
+                {/* First Link */}
+                <a href="#" className="footer__social-link">
+                  <svg className="footer__social-icon footer__social-icon-1">
+                    <use xlinkHref="assets/shared/desktop/facebook.svg#facebook"></use>
+                  </svg>
+
+                  <svg
+                    style={{
+                      width: "0",
+                      height: "0",
+                      position: "absolute",
+                    }}
+                  >
+                    <linearGradient id="a" x1="100%" x2="0%" y1="0%" y2="100%">
+                      <stop offset="0%" stop-color="#63AFDB" />
+                      <stop offset="100%" stop-color="#6028F1" />
+                    </linearGradient>
+                  </svg>
+                </a>
+
+                {/* Second Link */}
+                <a href="#" className="footer__social-link">
+                  <svg className="footer__social-icon footer__social-icon-2">
+                    <use xlinkHref="assets/shared/desktop/youtube.svg#youtube"></use>
+                  </svg>
+
+                  <svg
+                    style={{
+                      width: "0",
+                      height: "0",
+                      position: "absolute",
+                    }}
+                  >
+                    <linearGradient id="b" x1="0%" x2="100%" y1="100%" y2="0%">
+                      <stop offset="0%" stop-color="#D3205A" />
+                      <stop offset="100%" stop-color="#FF5A5A" />
+                    </linearGradient>
+                  </svg>
+                </a>
+
+                {/* Third Link */}
+                <a href="#" className="footer__social-link">
+                  <svg className="footer__social-icon footer__social-icon-3">
+                    <use xlinkHref="assets/shared/desktop/twitter.svg#twitter"></use>
+                  </svg>
+
+                  <svg
+                    style={{
+                      width: "0",
+                      height: "0",
+                      position: "absolute",
+                    }}
+                  >
+                    <linearGradient
+                      id="c"
+                      x1="100%"
+                      x2="0%"
+                      y1="16.979%"
+                      y2="83.021%"
+                    >
+                      <stop offset="0%" stop-color="#65FFEB" />
+                      <stop offset="100%" stop-color="#27718A" />
+                    </linearGradient>
+                  </svg>
+                </a>
+
+                {/* Fourth Link */}
+                <a href="#" className="footer__social-link">
+                  <svg className="footer__social-icon footer__social-icon-4">
+                    <use xlinkHref="assets/shared/desktop/pinterest.svg#pinterest"></use>
+                  </svg>
+
+                  <svg
+                    style={{
+                      width: "0",
+                      height: "0",
+                      position: "absolute",
+                    }}
+                  >
+                    <linearGradient id="d" x1="100%" x2="0%" y1="0%" y2="100%">
+                      <stop offset="0%" stop-color="#F6C683" />
+                      <stop offset="100%" stop-color="#DE3838" />
+                    </linearGradient>
+                  </svg>
+                </a>
+
+                {/* Fith Link */}
+                <a href="#" className="footer__social-link">
+                  <svg className="footer__social-icon footer__social-icon-5">
+                    <use xlinkHref="assets/shared/desktop/instagram.svg#instagram"></use>
+                  </svg>
+
+                  <svg
+                    style={{
+                      width: "0",
+                      height: "0",
+                      position: "absolute",
+                    }}
+                  >
+                    <linearGradient id="e" x1="0%" x2="50%" y1="100%" y2="0%">
+                      <stop offset="0%" stop-color="#FFC593" />
+                      <stop offset="51.945%" stop-color="#BC7198" />
+                      <stop offset="100%" stop-color="#5A77FF" />
+                    </linearGradient>
+                  </svg>
+                </a>
+              </div>
+            </div>
+
+            <div className="footer__middle-right">
+              <div className="footer__middle-nav">
+                <a href="/" className="footer__middle-nav-link">
+                  Home
+                </a>
+                <a href="#" className="footer__middle-nav-link">
+                  Stories
+                </a>
+                <a href="#" className="footer__middle-nav-link">
+                  Features
+                </a>
+                <a href="#" className="footer__middle-nav-link">
+                  Pricing
+                </a>
+              </div>
+
+              <div className="footer__right-nav">
+                <a href="#">
+                  <div className="footer__button">Get An Invite</div>
+                  <img
+                    className="footer__right-arrow"
+                    src="assets/shared/desktop/arrow.svg"
+                    alt="Right Arrow"
+                  />
+                </a>
+
+                <p className="footer__copyright">
+                  Copyright 2022. All Rights Reserved
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
